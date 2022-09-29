1350145772e2693976f8027c2ae366fbd2666aab
evgeny moskovskikh <mosevgwork@yandex.ru>
2022-09-29 19:26:38 +0300

    Методы навсегда

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,9 +4,18 @@ Console.Write("Введите число: ");
 int A = int.Parse(Console.ReadLine()!);
 Console.Write("Введите степень числа: ");
 int B = int.Parse(Console.ReadLine()!);
+int result = 1;
 void move(int A, int B)
 {
-    int result = Convert.ToInt32(Math.Pow(A,B));
-    Console.Write($"Число {A} в степени {B} равно {result}");
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
 }
 move(A, B);
+Console.WriteLine("Ответ: " + result);
+
+// {
+//     int result = Convert.ToInt32(Math.Pow(A,B));
+//     Console.Write($"Число {A} в степени {B} равно {result}");
+// }
